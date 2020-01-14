1030e575a22e70df8ac7894d408300647902e94c
Alex Aucestovar <aucestovara@gmail.com>
2020-01-14 13:19:49 -0600

    Regex validation in form fields

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,16 +3,24 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { PARTNERS } from '../shared/partners';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            
+            partners: PARTNERS,
+            comments: COMMENTS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -23,12 +31,15 @@ class Main extends Component {
                 <Home />
             );
         }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners}/>} />
+                    <Route exact path='/contactus' render={() => <Contact />} />
                 </Switch>
                 <Footer />
             </div>
